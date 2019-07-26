a51861ec64945da3e9ee3403ec38f9b0634031b6
user3 <user3@administrator-HP-Pro-3330-MT>
2019-07-26 11:45:01 +0530

    to operate my window

---
+++ src/mygui/Caluclation1.java
@@ -0,0 +1,14 @@
+package mygui;
+
+public class Caluclation1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+               MyWindow w1;
+               
+             w1=new MyWindow();
+             w1.setSize(100,100);
+             w1.setVisible(true);
+	}
+
+}
